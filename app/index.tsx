e6ca2cf5b6f114e2faa877e3e1e657cd9ff2bd6a
James Anwyl <ja.anwyl@gmail.com>
2025-01-17 21:51:48 +0000

    Install + run prettier

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,7 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
     </View>
   );
@@ -13,11 +11,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
   },
 });
